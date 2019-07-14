dbfccc6a606e61e0ebb48ec8e702aa757ccb0550
TheRealEldaria <brian@eldaria.net>
2019-07-14 19:48:30 +0200

    Extras to convert Torque Pro to AutoPi

---
+++ extras.py
@@ -0,0 +1,31 @@
+# Just some tools to convert 
+
+from string import ascii_uppercase
+import itertools
+
+
+def iter_all_strings():
+    for size in itertools.count(1):
+        for s in itertools.product(ascii_uppercase, repeat=size):
+            yield "".join(s)
+
+
+def find_letter(count):
+    if count < 3:
+        count = 3
+    for s in itertools.islice(iter_all_strings(), count-2):
+        pass
+    print(s)
+
+
+def find_number(letter):
+    count = 2
+    for s in iter_all_strings():
+        count += 1
+        if s == letter.upper():
+            print(count)
+            break
+
+find_letter(53)
+
+find_number("a")
